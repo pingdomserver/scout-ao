f7778a83a8050b55083a78df5fe1597b909af11a
Oskar Kapusta <oskar@librato.com>
2018-02-22 10:14:41 +0000

    Refactor

--- script/config.rb
+++ script/config.rb
@@ -1,7 +1,5 @@
 require 'erb'
 require 'pry'
-require 'net/http'
-require_relative './psm/api_client'
 
 class Configuration
   def initialize(opts = {})
@@ -10,23 +8,13 @@ class Configuration
     end
     @erb_template = ERB.new(File.read(
       File.expand_path(
-        "../../config/templates/config.yml.erb", __FILE__
+        "../../config/templates/config.yaml.erb", __FILE__
     )))
-    @key_processes = fetch_key_processes
   end
 
   def call
     puts @erb_template.result(binding)
   end
 
-  attr_reader :account_key, :environment, :hostname, :roles, :key_processes
-
-  def fetch_key_processes
-    client = Psm::ApiClient.new(account_key, hostname)
-    response = client.make_request("/api/v2/account/clients/processes")
-
-    response.map do |process|
-      process["name"]
-    end
-  end
+  attr_reader :account_key, :environment, :hostname, :roles
 end
