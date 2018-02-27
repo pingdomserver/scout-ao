be9c66ed53e2120308f424e34d6b235742399977
Oskar Kapusta <oskar@librato.com>
2018-02-27 09:23:16 +0000

    Add options to skip agen installation steps

---
+++ script/options.rb
@@ -0,0 +1,24 @@
+require 'optparse'
+
+class Options
+  def self.parse(args)
+    opts = {}
+
+    parser = OptionParser.new do |o|
+      o.on("--no-plugin") do |p|
+        opts[:skip_plugin] = true
+      end
+
+      o.on("--no-agent") do |a|
+        opts[:skip_agent] = true
+      end
+
+      o.on("--no-config") do |c|
+        opts[:skip_config] = true
+      end
+    end
+    parser.parse!(args)
+
+    opts
+  end
+end
--- script/runner.rb
+++ script/runner.rb
@@ -9,15 +9,17 @@ class Runner
   class NoApiKeyException < StandardError; end;
 
   class << self
-    def run(api_key)
+    def run(api_key, options)
+      # TODO: API call to AO to enable psm snap plugin
+
       unless api_key
         raise NoApiKeyException, "AppOptics Api key must be provided"
       end
 
-      Ao::Installer.call(api_key)
+      Ao::Installer.call(api_key) unless options[:skip_agent]
       client_configuration = Client.gather_facts
-      Plugin::Downloader.new(client_configuration).call
-      Configuration.new(client_configuration).call
+      Plugin::Downloader.new(client_configuration).call unless options[:skip_plugin]
+      Configuration.new(client_configuration).call unless options[:skip_config]
 
       # Install gems
 
@@ -27,6 +29,7 @@ class Runner
         FileUtils.cp File.expand_path("../../package/#{p}", __FILE__),
           "/opt/appoptics/bin/#{p}"
       end
+
       # Restart appoptics agent
 
       system "service appoptics-snapteld restart"
