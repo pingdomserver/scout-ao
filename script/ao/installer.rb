302f5ec9e2ec69fde3e575f2270fdb8a8d92e34c
Oskar Kapusta <oskar@librato.com>
2018-03-08 10:02:32 +0000

    Fix AO installer command

--- script/ao/installer.rb
+++ script/ao/installer.rb
@@ -6,7 +6,7 @@ module Ao
           "../../appoptics-host-agent-installer.sh", __FILE__
         )
         system %(export APPOPTICS_INSTALL_ONLY=1)
-        system %(bash -c '#{script_path}' --token #{api_key})
+        system %(bash #{script_path} --token #{api_key})
       end
     end
   end
