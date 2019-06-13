aca98acfd3c8f43bec64eab214504e5151703dd6
lootek <piotr.lucinski@solarwinds.cloud>
2019-06-13 09:24:20 +0200

    logging

--- migrate.rb
+++ migrate.rb
@@ -7,12 +7,19 @@ require_relative "lib/snap/config"
 
 options = Options.parse(ARGV)
 
+puts "Deactivate Scout"
 Scout.deactivate
 
+puts "Download PSM plugins"
 Scout.download_plugins unless options[:skip_plugins]
 
 unless options[:skip_config]
+  puts "Stop Snap Agent"
   SnapService.stop
+
+  puts "Configure Snap Agent"
   SnapConfig.reconfigure
+
+  puts "Start Snap Agent"
   SnapService.start
 end
